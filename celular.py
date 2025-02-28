3f4037d0a4f38e2cfb163c10aee802a5a2f954b3
GabeBersani <gabi-fernanda1@outlook.com>
2025-02-28 15:18:00 -0300

    poo

--- celular.py
+++ celular.py
@@ -15,29 +15,56 @@ class Eletronico:
         print(f'Estudo: {self.eletronico_estudo}')
 
 
-    def entretar(self):
-        if self.eletronico_entreterimento and self.eletronico_estudo:
-            print(f'É um eletronico de multitarefas')
-        elif self.eletronico_entreterimento:
+    def verificar_entreterimento(self):
+        if self.eletronico_entreterimento:
             print(f'É um eletronico de entreterimento')
         else:
             print(f'Não é um eletronico de entreterimento')
 
 
-    def estudar(self):
+    def verificar_estudo(self):
         if self.eletronico_estudo:
+            print(f'É um eletronico de estudo')
+        else:
             print(f'Não é um eletronico de estudo')
 
+    def estudar(self, status):
+        if self.eletronico_estudo and status:
+            print("Ele já é um eletronico de estudo")
+        elif not self.eletronico_estudo and not status:
+            print("Não é um eletronico de estudo")
+            self.eletronico_estudo()
+        elif not self.eletronico_estudo and status:
+            self.eletronico_estudo = status
+            print("Agora é um eletronico de estudo")
         else:
-            print(f'É um eletronico de estudo')
-            self.eletronico_estudo = True
+            self.eletronico_estudo = status
+            self.eletronico_estudo()
+
+
+
+    def entreter(self, status):
+        if self.eletronico_entreterimento and status:
+            print("É um eletronico de entreterimento")
+        elif not self.eletronico_entreterimento and not status:
+            print("Não é um eletronico de entreterimento")
+            self.eletronico_entreterimento()
+        elif not self.eletronico_entreterimento and status:
+            self.eletronico_entreterimento = status
+            print("É um eletronico de entreterimento")
+        else:
+            self.eletronico_entreterimento = status
+            self.eletronico_entreterimento()
 
 
 
 e1= Eletronico("R$400,00", "rosa", "pequeno")
 e1.apresentar()
-e1.estudar()
-e1.estudar()
+e1.estudar(True)
+e1.estudar(True)
+e1.entreter(True)
+e1.entreter(True)
+
 print("-" * 30)
 
 
@@ -63,29 +90,43 @@ class Celular(Eletronico):
             print(f'O Celular esta sem memoria')
 
 
-    def carregar(self):
-        if self.bateria and self.memoria:
-            print(f'O Celular esta com bateria e memoria')
 
-        elif self.bateria:
-            print("O Celular esta sem bateria")
+    def carregar(self, status):
+        if self.bateria and status:
+            print("O celular já esta com bateria")
+        elif not self.bateria and not status:
+            print("O celular esta sem bateria")
+            self.bateria()
+        elif not self.bateria and status:
+            self.bateria = status
+            print("Agora o celular esta com bateria")
         else:
-            print(f'O Celular esta com bateria')
-            self.bateria = True
+            self.bateria = status
+            self.bateria()
 
-    def mamoria(self):
-        if self.memoria:
-            print("O Celular esta sem memoria")
-            self.memoria = False
+
+
+
+
+    def armazenamento(self, status):
+        if self.memoria and status:
+            print("O celular esta com memoria")
+        elif not self.memoria and not status:
+            print("O celular esta sem memoria")
+            self.memoria()
+        elif not self.memoria and status:
+            self.memoria = status
+            print("Agora o celular esta com memoria")
         else:
-            print(f'O Celular esta com memoria')
-            self.memoria = True
+            self.memoria = status
+            self.memoria()
 
 
 
 c1 = Celular("R$400,00", "rosa", "medio")
 c1.apresentar()
-c1.carregar()
-c1.carregar()
+c1.carregar(True)
+c1.carregar(True)
+c1.armazenamento(True)
 
 
